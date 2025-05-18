e97ceb43ccd69ea3c2503590d5c06bd1e4b2bf9e
Aline Lima <153766003+alinaolima@users.noreply.github.com>
2025-05-17 21:30:17 -0400

    Create area.py
    Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
    a) a área do triângulo retângulo que tem A por base e C por altura.
    b) a área do círculo de raio C. (pi = 3.14159)
    c) a área do trapézio que tem A e B por bases e C por altura.
    d) a área do quadrado que tem lado B.
    e) a área do retângulo que tem lados A e B.

---
+++ beginner_level/area.py
@@ -0,0 +1,24 @@
+import math
+
+#lendo todos os valores em uma só linha
+valores = input ()
+
+#separando os valores
+numeros = valores.split()
+
+#atribuindo cada separação a variável
+A = float(numeros[0])
+B = float(numeros[1])
+C = float(numeros[2])
+
+areaTriangulo = (A * C) / 2.0
+areaCirculo = 3.14159 * pow(C, 2)
+areaTrapezio = ((A + B) * C) / 2.0
+areaQuadrado = pow(B,2)
+areaRetangulo = A * B
+
+print(f'TRIANGULO: {areaTriangulo:.3f}')
+print(f'CIRCULO: {areaCirculo:.3f}')
+print(f'TRAPEZIO: {areaTrapezio:.3f}')
+print(f'QUADRADO: {areaQuadrado:.3f}')
+print(f'RETANGULO: {areaRetangulo:.3f}')
