6bee9107ad65fd2f20125024271586fa4dca29e2
Aline Lima <153766003+alinaolima@users.noreply.github.com>
2025-05-17 21:49:07 -0400

    Update oMaior.py
    alteração do // para não precisar formatar na impressão e adição de outra forma de leitura

--- beginner_level/oMaior.py
+++ beginner_level/oMaior.py
@@ -6,7 +6,10 @@ a = numeros[0]
 b = numeros[1]
 c = numeros[2]
 
-maiorAB = (a + b + abs(a-b)) / 2
-ehmaior = (maiorAB + c + abs(maiorAB-c)) / 2
+maiorAB = (a + b + abs(a-b)) // 2
+ehmaior = (maiorAB + c + abs(maiorAB-c)) // 2
 
-print(f'{ehmaior:.0f} eh o maior')
+print(f'{ehmaior:} eh o maior')
+
+# outra forma de leitura
+# a,b,c = map(int, input().split())
